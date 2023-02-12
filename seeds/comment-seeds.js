55040b02c8d629e76ef406bdc13732e22f35a1af
Lon Heligas <lon.heligas@gmail.com>
2023-02-12 11:14:29 -0600

    more setup

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -6,6 +6,18 @@ const CategoryData = [
     "blog_id": 1,    
     "commentauthor_id": 2,
     "comment_body": "I like pie."
+  },
+  {
+    "id": 2,
+    "blog_id": 2,    
+    "commentauthor_id": 1,
+    "comment_body": "I don't like pie."
+  },
+  {
+    "id": 3,
+    "blog_id": 3,    
+    "commentauthor_id": 3,
+    "comment_body": "I like cake."
   }
 ]
 
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = 3001;
 
 
 app.use(express.urlencoded({ extended: true }));
